1e79579a4f324be65fa00f37f0a86baba0fcc8af
halvaradop_ <hernanvid123@gmail.com>
2023-06-27 19:02:01 -0500

    addition of current page index

--- src/components/CardSelectPlan.tsx
+++ src/components/CardSelectPlan.tsx
@@ -1,8 +1,8 @@
 import { CardSelectPlanProps } from "../interfaces/types"
 
-const CardSelectPlan = ({ img, title, price, children, onClick }: CardSelectPlanProps) => {
+const CardSelectPlan = ({ className = "", img, title, price, children, onClick }: CardSelectPlanProps) => {
     return (
-        <article className="card-plans items-start justify-start gap-x-5 is-plan:border-blue-100 is-plan:bg-magnolia hover:cursor-pointer sm:p-5" onClick={onClick}>
+        <article className={`card-plans items-start justify-start gap-x-5 is-plan:border-blue-100 is-plan:bg-magnolia hover:cursor-pointer sm:p-5`} onClick={onClick}>
             <img src={img} alt="icon of a plan" />
             <div>
                 <h2 className="text-blue text-xl/5 font-bold">{title}</h2>
---
+++ src/components/IndexSection.tsx
@@ -0,0 +1,15 @@
+import { IndexSectionProps } from "../interfaces/types"
+
+const IndexSection = ({ index, title }: IndexSectionProps) => {
+    return (
+        <div className="base:flex base:items-center base:gap-x-5">
+            <span className="w-8 h-8 text-white font-bold grid place-content-center border border-white rounded-full">{index}</span>
+            <div className="hidden base:uppercase base:block">
+                <p className="text-gray-100 text-sm/4 font-medium">Step {1}</p>
+                <h2 className="text-white text-base font-bold">{title}</h2>
+            </div>
+        </div>
+    )
+}
+
+export { IndexSection }
